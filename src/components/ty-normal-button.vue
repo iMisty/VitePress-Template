47aa35252bb55dd6e0f901b9586d2df4755d26dd
Miya <qianwumeng_2cyndi@qq.com>
2019-04-20 00:38:52 +0800

    Create ty-normal-button.vue

---
+++ src/components/ty-normal-button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <a :href="links" target="_blank">
+            <button>
+                <i class="fa" :class="icon"> {{ message }}</i>
+            </button>
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: ['links','icon','message'],
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () {},
+  mounted () {}
+}
+</script>
+<style lang="less" scoped>
+div{
+    display: flex;
+}
+</style>
