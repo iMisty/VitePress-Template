db05ea9a584549722bf66b7de1da30fbd499a35b
Miya <qianwumeng_2cyndi@qq.com>
2019-06-19 20:22:40 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,29 @@
 <template>
-    <div id="app">
-        <div 
-            id="nav" 
-            :class="navBarFixed == true ? 'isFixed' : ''">
-            <logo
-                :class="navBarColorFixed == true ? 'colorFixed' : ''"/>
-            <navigation
-                :class="navBarColorFixed == true ? 'colorFixed' : ''"/>
-        </div>
-        <section-header/>
-        <section-wrap/>
-        <section-footer/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-import logo from './components/logo';
-import navigation from './components/navigation';
-import Header from './components/header';
-import wrap from './components/wrap';
-import Footer from './components/footer';
-
-export default {
-    name: 'App',
-    components: {
-        logo,
-        navigation,
-        'section-header': Header,
-        'section-wrap': wrap,
-        'section-footer': Footer
-    },
-    data(){
-        return{
-            navBarFixed: false,
-            navBarColorFixed: false
-        }
-    },
-    mounted() {
-        window.addEventListener('scroll', this.handleScroll);
-    },
-    methods: {
-        handleScroll() {
-            let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
-
-            const offsetTop = document.querySelector('#app').offsetTop;
-
-            if (scrollTop > offsetTop) {
-                this.navBarFixed = true;
-                this.navBarColorFixed = true;
-            } else {
-                this.navBarFixed = false;
-                this.navBarColorFixed = false;
-            }
-        }
-    },
-    destroyed() {
-        window.removeEventListener('scroll', this.handleScroll);  
-    },
-}
-</script>
-
 <style lang="less">
-#app{
-    margin: 0;
-    padding: 0;
-    background-color: #e6e6e6;
-    font-size: 1rem;
-    letter-spacing: 1px;
-    overflow-x: hidden;
-    #nav{
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-        position: fixed;
-        width: 100%;
-        transition: .4s;
-    }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
