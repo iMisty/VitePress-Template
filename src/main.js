27e1b38b8ec28ed140f4c57b5a33c2cb17cbf470
Miya <qianwumeng_2cyndi@qq.com>
2019-07-04 23:28:09 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'font-awesome/css/font-awesome.css'
-import './assets/global.css'
-//import './assets/function'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
