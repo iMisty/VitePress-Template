d7a14fd8e2278a1859b8fb76486ee37b1f13553c
Kagura Koeri <qianwumeng_2cyndi@qq.com>
2022-09-05 21:46:28 +0800

    :lipstick: Create Template

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,8 +3,8 @@
  * @Version: 1.0
  * @Author: Mirage
  * @Date: 2022-06-28 17:03:27
- * @LastEditors: Mirage
- * @LastEditTime: 2022-06-29 16:09:07
+ * @LastEditors: Miya
+ * @LastEditTime: 2022-09-05 21:45:02
  */
 import { defineConfig } from 'vitepress';
 // https://vitepress.vuejs.org/config/theme-configs.html#nav
@@ -21,7 +21,7 @@ const config = defineConfig({
   title: 'VitePress',
   description: 'Vite & Vue powered static site generator.',
   appearance: true,
-  base: '/',
+  base: '/VitePress-Template',
   themeConfig: {
     logo: '/favicon.ico',
     socialLinks,
